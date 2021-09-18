2495693d499cb7c6de53fcc366deb8ef5fc462ea
ArvandAlfarizki <arvand.rizki01@gmail.com>
2021-09-18 09:57:54 +0700

    Chapter 4 - Tower Defense 2D

---
+++ Tower Defense 2D/Assets/Scripts/TimerSetting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerSetting : MonoBehaviour
+{
+    public Text TextTimer;
+    public float Waktu = 100;
+
+    public bool Timer = true;
+
+    void SetText()
+    {
+        int Menit = Mathf.FloorToInt(Waktu / 60);
+        int Detik = Mathf.FloorToInt(Waktu % 60);
+        TextTimer.text = Menit.ToString("00")+":"+ Detik.ToString("00");
+    }
+    
+    float s;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+        if (Timer)
+        {
+                s += Time.deltaTime;
+            if(s >=1)
+            {
+                Waktu++;
+                s = 0;
+            }
+        }
+
+        if(Timer && Waktu >= 200)
+        {
+            Debug.Log("TimeOut");
+            Timer = false;
+        }
+
+        SetText();
+
+    }
+}
